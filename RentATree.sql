681a8c2bb08b2229bb614a97770aaca7b86761f8
harisonw <87620631+harisonw@users.noreply.github.com>
2021-11-03 17:53:13 +0000

    Update RentATree.sql

--- RentATree.sql
+++ RentATree.sql
@@ -106,12 +106,18 @@ create procedure login(
 begin
 	declare Password_Login varchar(300); -- Declare local variable
     declare encrypted_Password_Attempt varchar(300); -- Delcare local variable
+	DECLARE isAdmin BOOL;
     set Password_Login = (select (UserDetailsMaster.Password) from UserDetailsMaster where UserDetailsMaster.Username = p_Username); -- Set it equal to the password of corresponding username
     set encrypted_Password_Attempt = SHA1(p_Password); -- Checks if whatever password is attempted to log in is equal to the one stored
-    if Password_Login = encrypted_Password_Attempt AND isAdmin = TRUE then 
-		set p_result = 2;
-    elseif Password_Login = encrypted_Password_Attempt then 
-		set p_Result = 1; -- If the password is correct then set result to 1
+    SET isAdmin = (SELECT (UserDetailsMaster.isAdmin) FROM UserDetailsMaster WHERE UserDetailsMaster.Username = p_Username);
+  
+
+    if Password_Login = encrypted_Password_Attempt then 
+		IF isAdmin THEN
+			SET p_Result = 2;
+		ELSE
+			set p_Result = 1; -- If the password is correct then set result to 1
+		END IF;
 	else 
 		set p_Result = 0; -- Otherwise, set result to 0
 	end if;
@@ -137,7 +143,7 @@ begin
 	DECLARE isAdmin BOOL;
     set Password_Login = (select (UserDetailsMaster.Password) from UserDetailsMaster where UserDetailsMaster.Username = p_Username); -- Set it equal to the password of corresponding username
     set encrypted_Password_Attempt = SHA1(p_Password); -- Checks if whatever password is attempted to log in is equal to the one stored
-    SET isAdmin = (SELECT Admin FROM UserDetailsMaster WHERE UserDetailsMaster.Username = p_Username);
+    SET isAdmin = (SELECT (UserDetailsMaster.isAdmin) FROM UserDetailsMaster WHERE UserDetailsMaster.Username = p_Username);
     if Password_Login = encrypted_Password_Attempt AND isAdmin = TRUE then 
 		set p_Result = 1; -- If the password is correct then set result to 1
 	else 
